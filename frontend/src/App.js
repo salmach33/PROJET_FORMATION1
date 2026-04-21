2cf490be4edd42331237463129c97f94982e040e
Salma33 <salmachlarhmo@gmail.com>
2026-04-21 17:52:23 +0100

    add frontend correctly

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [formations, setFormations] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/formations")
+      .then(res => res.json())
+      .then(data => setFormations(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Liste des formations</h1>
+      <ul>
+        {formations.map((f, index) => (
+          <li key={index}>{f.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
