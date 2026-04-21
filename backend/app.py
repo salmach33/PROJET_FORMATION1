0383d2a06c263b9edfb620806160bd310e89b2bd
Salma33 <salmachlarhmo@gmail.com>
2026-04-21 17:35:57 +0100

    first commit

---
+++ backend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/formations_db"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Bienvenue API Flask"})
+
+@app.route("/formations")
+def formations():
+    return jsonify([
+        {"title": "Docker"},
+        {"title": "React"},
+        {"title": "Flask"}
+    ])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
