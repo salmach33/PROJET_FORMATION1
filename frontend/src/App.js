82c2d90a46a4690db251d0c45296a94be2d3affb
Salma33 <salmachlarhmo@gmail.com>
2026-04-22 09:57:21 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div>
-      <h1>Liste des formations</h1>
+      <h1>Liste des formations test</h1>
       <ul>
         {formations.map((f, index) => (
           <li key={index}>{f.title}</li>
